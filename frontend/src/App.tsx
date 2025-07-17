3c53140ce27380925ea8a8cbafc35bcded48ec60
eduardo <eduardo@int6tech.com.br>
2025-07-17 18:24:37 -0300

    Current Project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Login from './components/Login';
 
 function App() {
   return (
-      <Header/>
+    <> 
+      <Header/>      
+      <Login/>
+      </>
   );
 }
 
---
+++ frontend/src/components/LoginCard.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const LoginCard : React.FC = () => (
+    <>
+    
+    </>
+);
+
+export default LoginCard
