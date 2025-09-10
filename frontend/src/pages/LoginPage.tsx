aeadfdf2b35bd23a99eee573cf9c027edcd7424e
EduardoCFonte <dudufonte0@gmail.com>
2025-09-09 22:06:21 -0300

    Changing front-end, creating routes

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import BackgroundImage from "../assets/FundoLogin.jpeg"
+import LoginCard from "../components/LoginCard"
+import Header from "../components/Header";
+
+
+const LoginPage : React.FC = () => (
+    <main
+    style={{ backgroundImage: `url(${BackgroundImage})` }}
+    className="h-screen bg-cover bg-center flex items-center justify-center"
+    >
+        <div >
+            <LoginCard/>        
+        </div>
+    
+    </main>
+);
+
+export default LoginPage;
