1d92caf4089acbb1216582373c5e54bc4dce171e
EduardoCFonte <dudufonte0@gmail.com>
2025-09-17 20:28:12 -0300

    Merged everything to a single validator file, created email validator

--- frontend/src/utils/validatePassword.ts
+++
@@ -1,7 +0,0 @@
-export const validatePassword = (value: string): string => {
-    if (value.length < 8) return "A senha deve ter no mínimo 8 caracteres.";
-    if (!/[a-z]/.test(value)) return "A senha deve conter pelo menos uma letra minúscula.";
-    if (!/[A-Z]/.test(value)) return "A senha deve conter pelo menos uma letra maiúscula.";
-    if (!/[0-9]/.test(value)) return "A senha deve conter pelo menos um número.";
-    return ""; // Sem erro
-  };
